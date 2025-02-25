1a30df61fa999842da07b59c72f1fa1d008280d5
Tushar Pardhe <tpardhe@gmail.com>
2025-02-25 00:02:54 +0000

    chore: changed constant export

--- src/common/common.constants.js
+++ src/common/common.constants.js
@@ -1,3 +1,9 @@
-export const BASE_RESERVE_XRPL = 2;
-export const RESERVE_INCREMENT_XRPL = 0.2;
-export const XRPL_SERVER = 'wss://s.altnet.rippletest.net:51233';
+const BASE_RESERVE_XRPL = 2;
+const RESERVE_INCREMENT_XRPL = 0.2;
+const XRPL_SERVER = 'wss://s.altnet.rippletest.net:51233';
+
+module.exports = {
+    BASE_RESERVE_XRPL,
+    RESERVE_INCREMENT_XRPL,
+    XRPL_SERVER,
+};
